b227790d0fd6d4cfbaf8684128f67cd74bae4356
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-04-14 20:57:35 +0200

    implement new solution and simplify code
    add previous, current number and operator display

--- script.js
+++ script.js
@@ -1,109 +1,43 @@
-let firstNumber = 4;
-let secondNumber = 5;
-let operator = 0;
-let result = 0;
-
-if ((operator === '-' || operator === '+' || operator === 'X' || operator === '/') && firstNumber === 'number') {
-    displayValue.textContent = '';
-}
-
-let displayValue = document.querySelector('.display');
-const digits = document.querySelectorAll('.digits');
-const clearButton = document.querySelector('.clearButton');
-clearButton.addEventListener('click', allClear);
+const previousNumber = document.querySelector('.previousNumber');
+const currentNumber = document.querySelector('.currentNumber');
+const operator = document.querySelector('.operator');
 const operatorButtons = document.querySelectorAll('.mathButton');
 const equalsButton = document.querySelector('.equalsButton');
-equalsButton.addEventListener('click', updateSecondNumber);
+const digits = document.querySelectorAll('.digits')
+const clearButton = document.querySelector('.clearButton');
 
-for (let i = 0; i < operatorButtons.length; i ++) {
+for (let i = 0; i < operatorButtons.length; i++) {
     const operatorButton = operatorButtons[i];
-    operatorButton.addEventListener('click', chooseOperator);
+    operatorButton.addEventListener('click', operate)
 }
-
 
 for (let i = 0; i < digits.length; i++) {
     const digit = digits[i];
-    digit.addEventListener('click', changeDisplay);
+    digit.addEventListener('click', displayNumbers);
 }
 
-function changeDisplay() {
-    let digitPressed = this.innerHTML;
-    displayValue.textContent = displayValue.textContent + digitPressed
-    // digits.style.color = 'red';
-    console.log(displayValue.textContent);
-    return;
+equalsButton.addEventListener('click', showResult);
+clearButton.addEventListener('click', clearScreen);
+
+let result = '';
+
+function displayNumbers() {
+    currentNumber.innerHTML += this.textContent;
+    
 }
 
-function allClear() {
-    displayValue.textContent = '';
+function operate() {
+previousNumber.innerHTML = currentNumber.innerHTML;
+operator.innerHTML = this.innerHTML;
+currentNumber.innerHTML = '';
 }
 
-function chooseOperator() {
-    firstNumber = updateFirstNumber();
-    console.log(firstNumber);
-    let operatorChoice = this.innerHTML;
-    switch(operatorChoice) {
-        case '+':
-            operator = '+';
-            displayValue.textContent = '';
-            break;
-        case '-':
-            operator = '-';
-            displayValue.textContent = '';
-            break;
-        case 'X':
-            operator = 'X';
-            displayValue.textContent = '';
-            break;
-        case '/':
-            operator = '/';
-            displayValue.textContent = '';
-            break;
-        default:
-            operator
-    }
-}
+function showResult() {
+  
+};
 
-function updateFirstNumber() {
-    let firstNumber = Number(displayValue.textContent);
-    console.log(typeof firstNumber, firstNumber + ' is a firstNumber');
-    displayValue.textContent = '';
-    return firstNumber;
-}
-
-function add(firstNumber, secondNumber) {
-    result = firstNumber + secondNumber;
-    displayValue.textContent = result;
-    return result;
-}
-
-function subtract(firstNumber, secondNumber) {
-    result = firstNumber - secondNumber;
-    displayValue.textContent = result;
-    return result;
-}
-
-function multiply(firstNumber, secondNumber) {
-    result = firstNumber * secondNumber;
-    displayValue.textContent = result;
-    return result;
-}
-
-function divide(firstNumber, secondNumber) {
-    result = firstNumber / secondNumber;
-    displayValue.textContent = result;
-    return result;
-}
-
-function makeCalc(firstNumber, operator, secondNumber) {
-    if (operator === '+') console.log(add(firstNumber, secondNumber));
-    else if (operator === '-') subtract(firstNumber, secondNumber);
-    else if (operator === 'X') multiply(firstNumber, secondNumber);
-    else if (operator === '/') divide(firstNumber, secondNumber);   
-}
-
-function updateSecondNumber() {
-    secondNumber = Number(displayValue.textContent);
-    console.log(typeof secondNumber, secondNumber + ' is the second number');
-    makeCalc(firstNumber, operator, secondNumber);
+function clearScreen() {
+    currentNumber.innerHTML = '';
+    previousNumber.innerHTML = '';
+    operator.innerHTML = '';
 }
